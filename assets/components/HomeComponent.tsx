da6845fb7f69e7cee5edf44e5a6af89d5146f3b5
ErnestasK760 <ernisq144@gmail.com>
2023-02-04 05:49:24 +0200

    Fixed with apache-pack, profiler now working added home react routing added Home/navbar component

---
+++ assets/components/HomeComponent.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+export const HomeComponent: FunctionComponent = () => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+};
--- assets/main.tsx
+++ assets/main.tsx
@@ -1,4 +1,21 @@
+import React, { FunctionComponent } from 'react';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { HomeComponent } from './components/HomeComponent';
+import ReactDOM from 'react-dom';
 import './styles/app.css';
-
-// start the Stimulus application
+import './styles/app.scss';
 import './bootstrap';
+
+const App: FunctionComponent = () => {
+    return (
+        <Router>
+        <HomeComponent />
+        </Router>
+    );
+};
+
+const container = document.getElementById('root');
+// @ts-ignore
+const root = createRoot(container);
+root.render(<App />);
